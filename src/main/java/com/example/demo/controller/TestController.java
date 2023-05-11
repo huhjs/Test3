2df8aec5ccc407b55542fd5bf871fa63b81548ba
huhjs <xxxyzzzky@gmail.com>
2023-05-11 10:02:43 +0900

    test1 브랜치에서 작업

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,11 +3,18 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class TestController {
-	@GetMapping("/")
-	public String index() {
-		System.out.println("강의실 작업");
-		return "index";
+	@Controller
+	public class TestController {
+		@GetMapping("/")
+		public String index() {
+			System.out.println("강의실 작업");
+			return "index";
+		}
+		
+		
+	public void test1() {
+		System.out.println("test1 branch에서 작업");
 	}
 }
+
+	
